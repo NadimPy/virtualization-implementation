5cd10396157a405fd4bf00c31f5b6b1b89057bd3
NadimPy <nadimchendy2@gmail.com>
2026-02-20 00:28:44 +0100

    Prepare workspace for go migration

--- auth_service.py
+++
@@ -1,8 +0,0 @@
-from fastapi import Header, HTTPException
-from SQL.USERS_related import get_user_by_api_key
-
-async def get_current_user(x_api_key: str = Header(..., alias="X-API-Key")):
-    user = get_user_by_api_key(x_api_key)
-    if not user:
-        raise HTTPException(401, "Invalid API key")
-    return user
